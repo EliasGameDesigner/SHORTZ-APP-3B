80bcc88707eb05b0d0bae347eda94cdd293f869f
ELIAS SCHROEDER DE SOUZA <el.souza@catolicasc.edu.br>
2026-03-05 22:23:35 -0300

    alteracao final da aula 3

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const flash = require('connect-flash')
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,6 +18,22 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie :{maxAge: 1000*60*80*24}
+}));
+
+app.use(flash());
+app.use((req, res, next) => {
+ res.locals.messages = req.flash();
+ next();
+}) ;
+
+
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
